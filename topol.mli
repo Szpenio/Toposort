bf3889d3e97896d371b926e6c1c0389d7094590c
Szpenio <58612067+Szpenio@users.noreply.github.com>
2020-01-07 09:29:58 +0100

    Update topol.mli

--- topol.mli
+++ topol.mli
@@ -1,10 +1,8 @@
-(** Sortowanie topologiczne *)
+(** Topological sort **)
 
 exception Cykliczne
-(** wyjatek rzucany przez [topol] gdy zaleznosci sa cykliczne *)
+(** exception raised in case that dependencies are cyclic **)
 
 val topol : ('a * 'a list) list -> 'a list
-(** Dla danej listy [(a_1,[a_11;...;a_1n]); ...; (a_m,[a_m1;...;a_mk])]
-    zwraca liste, na ktorej kazdy z elementow a_i oraz a_ij wystepuje
-    dokladnie raz i ktora jest uporzadkowana w taki sposob, ze kazdy
-    element a_i jest przed kazdym z elementow a_i1 ... a_il *)
+(** for a list [(a1, [a11, a12, ...]); ...] where elements [a11, a12, ...]
+    are dependent on a1 etc. returns list of elements sorted topologically **)
